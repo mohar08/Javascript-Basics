1eac48e3c0d584a79253ba9763b00076588328d9
Harshad Patil <harsh.patil317.hp@gmail.com>
2022-07-08 09:35:17 +0530

    Strict comparison 4 cases solved

---
+++ Comparisonstrictequality.js
@@ -0,0 +1,15 @@
+let a = 2 ;
+let b = 22 ;
+console.log(a === b);
+
+let c = 22 ;
+let d = 22 ;
+console.log(c === d);
+
+let e = 'Sum' ;
+let f = 'Summ' ;
+console.log(e === f);
+
+let g = 'Sum' ;
+let h = 'Sum' ;
+console.log(g === h);
