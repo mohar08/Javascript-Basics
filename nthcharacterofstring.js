97f6928bc9b34fee7cad8ba64480f0a521060b75
Harshad Patil <harsh.patil317.hp@gmail.com>
2022-07-08 09:49:32 +0530

    All characters considered and found

---
+++ nthcharacterofstring.js
@@ -0,0 +1,15 @@
+let Career = 'webdevlopment';
+
+console.log( Career[0]);
+console.log( Career[1]);
+console.log( Career[2]);
+console.log( Career[3]);
+console.log( Career[4]);
+console.log( Career[5]);
+console.log( Career[6]);
+console.log( Career[7]);
+console.log( Career[8]);
+console.log( Career[9]);
+console.log( Career[10]);
+console.log( Career[11]);
+console.log( Career[12]);
